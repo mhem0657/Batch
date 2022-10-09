b6947aa060cbfe3f5e686678c580526276472ddc
mhem0657 <114923216+mhem0657@users.noreply.github.com>
2022-10-09 18:51:51 +1100

    Update NGROK-AP.bat

--- NGROK-AP.bat
+++ NGROK-AP.bat
@@ -1,5 +1,5 @@
 C:
 cd C:\Users\runneradmin\.ngrok2
-echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
+echo authtoken: 2Fckz3vqXQWQzudelB3tS5A6MZh_6Q8ZWCD8UiBiP4DGyXMue & echo region: ap >> ngrok.yml & echo tunnels: >> ngrok.yml & echo     default: >> ngrok.yml & echo         proto: tcp >> ngrok.yml & echo         addr: 3389 >> ngrok.yml
 copy ngrok.yml C:\Windows\System32
 nssm install ngrok C:\Windows\System32\ngrok.exe start --all --config="C:\Windows\System32\ngrok.yml"
